31541a8bdcf03c342865ea663a0f856754d39d84
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2023-09-14 09:38:43 +0200

    leetCode problem solving

---
+++ Two sum/solution.js
@@ -0,0 +1,18 @@
+/* 
+   Test Cases
+    Input: nums = [2,7,11,15], target = 9
+    Output: [0,1]
+    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1]. 
+*/
+
+var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i+1; j < array.length; j++) {
+            if(nums[i] + nums[j]) {
+                return [i,j];
+            }
+            
+        }
+        
+    }
+};
