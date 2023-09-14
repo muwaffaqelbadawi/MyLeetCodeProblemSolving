6783c9d1a50f91eda93e38bc4c858f632ec242e3
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2023-09-14 11:46:58 +0200

    Hash Problems

--- Two sum/solution.js
+++ Two sum/solution.js
@@ -6,13 +6,30 @@
 */
 
 var twoSum = function (nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i+1; j < array.length; j++) {
-            if(nums[i] + nums[j]) {
-                return [i,j];
-            }
-            
-        }
-        
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
     }
+  }
 };
+
+var twoSumVersion2 = function (nums, target) {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    let value = nums[i];
+    let complementPair = target - value;
+    if (map[complementPair] !== undefined) {
+      return [map[complementPair], i];
+    } else {
+      map[value] = i;
+    }
+  }
+};
+
+const nums = [2, 7, 11, 15];
+const target = 9;
+const testCase = twoSumVersion2(nums, target);
+
+console.log(testCase);
