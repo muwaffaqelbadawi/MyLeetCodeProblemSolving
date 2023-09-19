92c06e8671af35017e542147a38fdbbb3f0988a6
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2023-09-19 11:48:05 +0200

    python .venv problem_solving

---
+++ problem_solving/longestSubstringWithoutRepeatingCharacter/solution.py
@@ -0,0 +1,22 @@
+
+# Time Complexity of generating all substring = O(n**3)
+
+class Solution:
+    def __init__(self) -> None:
+        # This is the constructor function
+        pass
+
+    def subarray(self, arr, listSize):
+        for i in range(0, listSize):
+            for j in range(0, listSize - i):
+                for k in range(i, i + j + 1):
+                    print(arr[k], end=" ")
+                print("\n", end="")
+
+
+# Drivar Program
+arr = [0, 1, 2, 3, 4]
+listSize = len(arr)
+
+solution1 = Solution()
+solution1.subarray(arr, listSize)
